cb3691580ffc9a557db833a76987176894389f63
kenzymoez <kenzymoez@gmail.com>
2026-07-03 11:43:09 +0300

    scramble

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\PostController;
@@ -13,3 +14,15 @@ use app\Http\Controllers\PostController;
 
 // Route::get('/posts', [PostController::class]);
 Route::resource('/posts', PostController::class);
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+//middleware -> checks lw el user logged in 34an mynfa3sh el user y3ml log out/refresh/view my profile w howa msh logged in
+// Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:api');
+// Route::post('/refresh',[AuthController::class,'refresh'])->middleware('auth:api');
+// Route::get('/user',[AuthController::class,'me'])->middleware('auth:api');
+
+Route::middleware('auth:api')->group(function(){
+Route::post('/logout',[AuthController::class,'logout']);
+Route::post('/refresh',[AuthController::class,'refresh']);
+Route::get('/user',[AuthController::class,'me']);
+});
